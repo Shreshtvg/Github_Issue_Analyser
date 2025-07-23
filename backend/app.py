13a17074670a5227a89698240fa37c9a39bc33f6
Shreshtvg <106970944+Shreshtvg@users.noreply.github.com>
2025-07-23 13:38:05 +0530

    Updated app.py with error handling

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from pydantic import BaseModel
 from backend.utils import fetch_issue_data, build_prompt, cache_result, get_cached_result
 from backend.model import analyze_issue
@@ -6,26 +6,37 @@ import hashlib
 
 app = FastAPI()
 
-#defining the type of parameters we expect in the request
 class IssueInput(BaseModel):
     repo_url: str
     issue_number: int
 
 @app.post("/analyze")
 def analyze(input: IssueInput):
+    print("Inside Backend")
     key = hashlib.md5(f"{input.repo_url}-{input.issue_number}".encode()).hexdigest()
+
     cached = get_cached_result(key)
     if cached:
+        print("Returning cached result")
         return {"cached": True, "result": cached}
 
+    print("Sending to fetch data")
     data = fetch_issue_data(input.repo_url, input.issue_number)
-    if "error" in data:
-        return {"error": data["error"]}
+
+    if "error1" in data:
+        # print("Issue number not valid in repo:", data["error"])
+        return {"error": data["error1"]}
+    if "error2" in data:
+        return {"error": data["error2"]}
 
     prompt = build_prompt(data)
+    print("Prompt received")
     result = analyze_issue(prompt)
-    cache_result(key, result)
-    print("Result from LLM:", result)
-    return {"cached": False, "result": result}
 
+    if "error3" in result:
+        print("Error in analysis:", result["error"])
+        return {"error": result["error3"]}
 
+    cache_result(key, result["response"])
+    print("Result from LLM:", result["response"])
+    return {"cached": False, "result": result["response"]}
