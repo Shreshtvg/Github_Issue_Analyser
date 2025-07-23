6ade5850c41565de8932c6f5fa7982c7fc57ffb2
Shreshtvg <106970944+Shreshtvg@users.noreply.github.com>
2025-07-23 13:39:41 +0530

    Updated model.py for error handling

--- backend/model.py
+++ backend/model.py
@@ -1,9 +1,10 @@
-# backend/model.py
-
 import os
 import json
 import requests
 from dotenv import load_dotenv
+
+# Load environment variables from .env
+
 
 API_URL = "https://router.huggingface.co/v1/chat/completions"
 
@@ -11,30 +12,30 @@ def analyze_issue(prompt: str) -> dict:
     print("Sending prompt to LLM for analysis...")
     load_dotenv()
     HF_TOKEN = os.getenv("HF_TOKEN")
-
+    print(HF_TOKEN)
     HEADERS = {
         "Authorization": f"Bearer {HF_TOKEN}",
         "Content-Type": "application/json"
     }
-    
-    try:
-        payload = {
-            "model": "moonshotai/Kimi-K2-Instruct:novita",
-            "messages": [
-                {"role": "user", "content": prompt}
-            ]
-        }
 
+    payload = {
+        "model": "moonshotai/Kimi-K2-Instruct:novita",
+        "messages": [
+            {"role": "user", "content": prompt}
+        ]
+    }
+
+    try:
         response = requests.post(API_URL, headers=HEADERS, json=payload)
         response.raise_for_status()
         data = response.json()
 
         if "choices" in data and data["choices"]:
-            return data["choices"][0]["message"]["content"]
+            return {"response": data["choices"][0]["message"]["content"]}
         else:
-            return {"error": "No response from model."}
+            return {"error3": "No response from model."}
 
     except requests.exceptions.RequestException as e:
-        return {"error": str(e)}
+        return {"error3": str(e)}
     except Exception as e:
-        return {"error": str(e)}
+        return {"error3": str(e)}
